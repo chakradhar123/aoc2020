faf395234454e112c65af85dae0ac772e931d8b0
chakradhar123 <chakradharvasurama@gmail.com>
2020-12-08 14:52:39 +0530

    day 8 complete

--- aocday8/aoc8a.py
+++ aocday8/aoc8a.py
@@ -5,48 +5,22 @@ while (line!='end'):
     line=input()
 
 
-def checkLines(lines):
+visited=[0 for line in lines]
 
-    visited=[0 for line in lines]
+curr=0
+acc=0
 
-    curr=0
-    acc=0
+while True:
 
-    while True:
-        if curr>=len(lines):
-       
-            return True,acc
-        if visited[curr]:
-            return False,0
-       
-
-        
-        visited[curr]=1
-        if lines[curr][0]=='acc':
-            acc=(acc+int(lines[curr][1][1:])) if (lines[curr][1][0]=='+') else (acc-int(lines[curr][1][1:]))
-            curr+=1
-            continue
-        if lines[curr][0]=='nop':
-            curr+=1
-            continue
-        curr=(curr+int(lines[curr][1][1:])) if (lines[curr][1][0]=='+') else (curr-int(lines[curr][1][1:]))
-    
-for line in lines:
-    if line[0]=='nop':
-        line[0]='jmp'
-        flag,acc=checkLines(lines)
-        if flag:
-            print(acc)
-            break
-        else:
-            line[0]='nop'
-        
-    if line[0]=='jmp':
-        line[0]='nop'
-        flag,acc=checkLines(lines)
-        if flag:
-            print(acc)
-            break
-        else:
-            line[0]='jmp'
-      
+    if visited[curr]:
+        print(acc)
+        break
+    visited[curr]=1;
+    if lines[curr][0]=='acc':
+        acc=(acc+int(lines[curr][1][1:])) if (lines[curr][1][0]=='+') else (acc-int(lines[curr][1][1:]))
+        curr+=1
+        continue
+    if lines[curr][0]=='nop':
+        curr+=1
+        continue
+    curr=(curr+int(lines[curr][1][1:])) if (lines[curr][1][0]=='+') else (curr-int(lines[curr][1][1:]))
