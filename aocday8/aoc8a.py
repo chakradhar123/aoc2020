0afa8a2bedeabd8d038542b61663b6ea18e5cf46
chakradhar123 <chakradharvasurama@gmail.com>
2020-12-11 12:40:59 +0530

    day 11 complete

--- aocday8/aoc8a.py
+++ aocday8/aoc8a.py
@@ -15,7 +15,7 @@ while True:
     if visited[curr]:
         print(acc)
         break
-    visited[curr]=1;
+    visited[curr]=1
     if lines[curr][0]=='acc':
         acc=(acc+int(lines[curr][1][1:])) if (lines[curr][1][0]=='+') else (acc-int(lines[curr][1][1:]))
         curr+=1
