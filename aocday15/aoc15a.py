ca8d15a066ad95bb9acf01c4ca8c1b94eae77e0f
chakradhar123 <chakradharvasurama@gmail.com>
2020-12-15 10:59:27 +0530

    day 15 complete

---
+++ aocday15/aoc15a.py
@@ -0,0 +1,19 @@
+lines=input().split(',')
+lineDict={}
+for i in range(0,len(lines)):
+    lines[i]=int(lines[i])
+    lineDict[lines[i]]=i+1
+lines.append(0)
+for i in range(len(lines),30000000):
+
+    if lines[i-1] in lineDict:
+        lines.append(i-lineDict[lines[i-1]])
+    else:
+        lines.append(0)
+
+    lineDict[lines[i-1]]=i
+
+print(lines[30000000-1])
+
+
+
